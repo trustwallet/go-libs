65717007d89d0e5095dd91b1b8f9e798242b59a7
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-12-09 11:08:35 +0800

    Add Middleware from golibs-networking (#8)
    * add middleware / mq
    
    * add sentry

---
+++ middleware/logger.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func Logger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	})
+}
