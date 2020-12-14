69995584ddf5b2275f78f3b942a280b3685c4dee
Maksim Rasputin <133312+madcake@users.noreply.github.com>
2020-12-15 02:57:42 +0700

    Add binance client (#13)
    * Add binance client
    
    * Add binance client
    
    * Improve client and clean code
    
    * Improve client and clean code
    
    * Add sugar
    Clean code
    
    * Add contract address
    
    * Update client.go
    
    * Update client.go
    
    * Update client.go
    
    Co-authored-by: Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>

---
+++ blockchain/binance/client.go
@@ -0,0 +1,51 @@
+package binance
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/trustwallet/golibs/client"
+)
+
+type Client struct {
+	req client.Request
+}
+
+func InitClient(url, apiKey string) Client {
+	request := client.InitJSONClient(url)
+	request.AddHeader("apikey", apiKey)
+	return Client{
+		req: request,
+	}
+}
+
+func (c Client) FetchNodeInfo() (result NodeInfoResponse, err error) {
+	err = c.req.Get(&result, "api/v1/node-info", nil)
+	return result, err
+}
+
+func (c Client) FetchTransactionsInBlock(blockNumber int64) (result TransactionsInBlockResponse, err error) {
+	err = c.req.Get(&result, fmt.Sprintf("api/v2/transactions-in-block/%d", blockNumber), nil)
+	return result, err
+}
+
+func (c Client) FetchTransactionsByAddressAndTokenID(address, tokenID string, limit int) ([]Tx, error) {
+	startTime := strconv.Itoa(int(time.Now().AddDate(0, -3, 0).Unix() * 1000))
+	params := url.Values{"address": {address}, "txAsset": {tokenID}, "startTime": {startTime}, "limit": {strconv.Itoa(limit)}}
+	var result TransactionsInBlockResponse
+	err := c.req.Get(&result, "api/v1/transactions", params)
+	return result.Tx, err
+}
+
+func (c Client) FetchAccountMeta(address string) (result AccountMeta, err error) {
+	err = c.req.Get(&result, fmt.Sprintf("api/v1/account/%s", address), nil)
+	return result, err
+}
+
+func (c Client) FetchTokens(limit int) (result Tokens, err error) {
+	query := url.Values{"limit": {strconv.Itoa(limit)}}
+	err = c.req.Get(&result, "api/v1/tokens", query)
+	return result, err
+}
