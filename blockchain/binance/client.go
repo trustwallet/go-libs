c01c28dd1eecda02bedd8ce3892892c9fff0ecd5
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2020-12-14 11:40:41 -0800

    Update client.go

--- blockchain/binance/client.go
+++ blockchain/binance/client.go
@@ -22,12 +22,12 @@ func InitClient(url, apiKey string) Client {
 }
 
 func (c Client) FetchNodeInfo() (result NodeInfoResponse, err error) {
-	err = c.req.Get(&result, "/api/v1/node-info", nil)
+	err = c.req.Get(&result, "api/v1/node-info", nil)
 	return result, err
 }
 
 func (c Client) FetchTransactionsInBlock(blockNumber int64) (result TransactionsInBlockResponse, err error) {
-	err = c.req.Get(&result, fmt.Sprintf("/api/v2/transactions-in-block/%d", blockNumber), nil)
+	err = c.req.Get(&result, fmt.Sprintf("api/v2/transactions-in-block/%d", blockNumber), nil)
 	return result, err
 }
 
@@ -35,17 +35,17 @@ func (c Client) FetchTransactionsByAddressAndTokenID(address, tokenID string, li
 	startTime := strconv.Itoa(int(time.Now().AddDate(0, -3, 0).Unix() * 1000))
 	params := url.Values{"address": {address}, "txAsset": {tokenID}, "startTime": {startTime}, "limit": {strconv.Itoa(limit)}}
 	var result TransactionsInBlockResponse
-	err := c.req.Get(&result, "/api/v1/transactions", params)
+	err := c.req.Get(&result, "api/v1/transactions", params)
 	return result.Tx, err
 }
 
 func (c Client) FetchAccountMeta(address string) (result AccountMeta, err error) {
-	err = c.req.Get(&result, fmt.Sprintf("/api/v1/account/%s", address), nil)
+	err = c.req.Get(&result, fmt.Sprintf("api/v1/account/%s", address), nil)
 	return result, err
 }
 
 func (c Client) FetchTokens(limit int) (result Tokens, err error) {
 	query := url.Values{"limit": {strconv.Itoa(limit)}}
-	err = c.req.Get(&result, "/api/v1/tokens", query)
+	err = c.req.Get(&result, "api/v1/tokens", query)
 	return result, err
 }
