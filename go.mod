0a26578208317cd4f9f266e6a523718ee81c1dd4
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-01-04 21:28:18 -0800

    Add SetupGracefulShutdown

--- go.mod
+++ go.mod
@@ -7,8 +7,6 @@ require (
 	github.com/imroc/req v0.3.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/shopspring/decimal v1.2.0
-	github.com/sirupsen/logrus v1.7.0 // indirect
-	github.com/streadway/amqp v1.0.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
---
+++ network/gin/setup.go
@@ -0,0 +1,43 @@
+package gin
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func SetupGracefulShutdown(ctx context.Context, port string, engine *gin.Engine) {
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: engine,
+	}
+
+	defer func() {
+		if err := server.Shutdown(ctx); err != nil {
+			log.Info("Server Shutdown: ", err)
+		}
+	}()
+
+	signalForExit := make(chan os.Signal, 1)
+	signal.Notify(signalForExit,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal("Application failed", err)
+		}
+	}()
+	log.WithFields(log.Fields{"bind": port}).Info("Running application")
+
+	stop := <-signalForExit
+	log.Info("Stop signal Received", stop)
+	log.Info("Waiting for all jobs to stop")
+}
