74e68ef98cfe531e66217fbbba2f060eaaa6df4e
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-01-18 22:46:42 -0800

    Add SentryErrorHandler

--- network/middleware/sentry.go
+++ network/middleware/sentry.go
@@ -1,7 +1,11 @@
 package middleware
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/evalphobia/logrus_sentry"
+	"github.com/getsentry/raven-go"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -19,5 +23,22 @@ func SetupSentry(dsn string) error {
 	hook.StacktraceConfiguration.Context = 10
 	hook.StacktraceConfiguration.SendExceptionType = true
 	log.AddHook(hook)
+	return nil
+}
+
+var SentryErrorHandler = func(res *http.Response, uri string) error {
+	statusCode := res.StatusCode
+	//Improve ways to identify if worth logging the error
+	if statusCode != http.StatusOK && statusCode != http.StatusNotFound {
+		log.WithFields(log.Fields{
+			"tags": raven.Tags{
+				{Key: "status_code", Value: strconv.Itoa(res.StatusCode)},
+				{Key: "host", Value: res.Request.URL.Host},
+				{Key: "url", Value: uri},
+			},
+			"fingerprint": []string{"client_errors"},
+		}).Error("Client Errors")
+	}
+
 	return nil
 }
