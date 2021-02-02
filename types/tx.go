a324cb63de90ac022227070e23b6c4c57cd67028
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-02-02 15:31:17 -0800

    Update tx.go

--- types/tx.go
+++ types/tx.go
@@ -238,16 +238,6 @@ func (txs Txs) FilterTransactionsByType(types []TransactionType) Txs {
 	return result
 }
 
-func (txs Txs) FilterTransactionsByTokenTransfersInContract() Txs {
-	result := make(Txs, 0)
-	for _, tx := range txs {
-		if tx.Type == TxContractCall && len(tx.TokenTransfers) > 0 {
-			result = append(result, tx)
-		}
-	}
-	return result
-}
-
 func AllowMemo(memo string) bool {
 	// only allows numeric values
 	_, err := strconv.ParseFloat(memo, 64)
