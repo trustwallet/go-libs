7428ba1a0cd8fad62bcb45d4ddac466e07909152
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-03-08 15:40:35 +0900

    Add optional transfer_fee to Collectible (#52)
    * add optional transfer_fee for eip1155
    * add CollectibleTransferFee struct

--- types/collectibles.go
+++ types/collectibles.go
@@ -15,20 +15,26 @@ type (
 
 	CollectionPage []Collection
 
+	CollectibleTransferFee struct {
+		Asset  string `json:"asset"`
+		Amount string `json:"amount"`
+	}
+
 	Collectible struct {
-		ID              string `json:"id"`
-		CollectionID    string `json:"collection_id"`
-		TokenID         string `json:"token_id"`
-		ContractAddress string `json:"contract_address"`
-		Category        string `json:"category"`
-		ImageUrl        string `json:"image_url"`
-		ExternalLink    string `json:"external_link"`
-		ProviderLink    string `json:"provider_link"`
-		Type            string `json:"type"`
-		Description     string `json:"description"`
-		Coin            uint   `json:"coin"`
-		Name            string `json:"name"`
-		Version         string `json:"nft_version"`
+		ID              string                 `json:"id"`
+		CollectionID    string                 `json:"collection_id"`
+		TokenID         string                 `json:"token_id"`
+		ContractAddress string                 `json:"contract_address"`
+		Category        string                 `json:"category"`
+		ImageUrl        string                 `json:"image_url"`
+		ExternalLink    string                 `json:"external_link"`
+		ProviderLink    string                 `json:"provider_link"`
+		Type            string                 `json:"type"`
+		Description     string                 `json:"description"`
+		Coin            uint                   `json:"coin"`
+		Name            string                 `json:"name"`
+		Version         string                 `json:"nft_version"`
+		TransferFee     CollectibleTransferFee `json:"transfer_fee,omitempty"`
 	}
 
 	CollectiblePage []Collectible
