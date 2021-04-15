4b877f65354a057619134de38dedc6d6d63da311
Ivan Bozhytksyi <82358585+ivan-bozhytksyi@users.noreply.github.com>
2021-04-15 11:16:45 +0300

    Extend Collectible model with typed media urls (#55)
    Co-authored-by: Ivan Bozhytskyi <ivan.bozhytskyi@binance.com>

--- types/collectibles.go
+++ types/collectibles.go
@@ -20,21 +20,28 @@ type (
 		Amount string `json:"amount"`
 	}
 
+	CollectibleMediaURL struct {
+		Mimetype string `json:"mimetype"`
+		URL      string `json:"url"`
+	}
+
 	Collectible struct {
-		ID              string                  `json:"id"`
-		CollectionID    string                  `json:"collection_id"`
-		TokenID         string                  `json:"token_id"`
-		ContractAddress string                  `json:"contract_address"`
-		Category        string                  `json:"category"`
-		ImageUrl        string                  `json:"image_url"`
-		ExternalLink    string                  `json:"external_link"`
-		ProviderLink    string                  `json:"provider_link"`
-		Type            string                  `json:"type"`
-		Description     string                  `json:"description"`
-		Coin            uint                    `json:"coin"`
-		Name            string                  `json:"name"`
-		Version         string                  `json:"nft_version"`
-		TransferFee     *CollectibleTransferFee `json:"transfer_fee,omitempty"`
+		ID                string                  `json:"id"`
+		CollectionID      string                  `json:"collection_id"`
+		TokenID           string                  `json:"token_id"`
+		ContractAddress   string                  `json:"contract_address"`
+		Category          string                  `json:"category"`
+		ImageUrl          string                  `json:"image_url"`
+		ExternalLink      string                  `json:"external_link"`
+		ProviderLink      string                  `json:"provider_link"`
+		Type              string                  `json:"type"`
+		Description       string                  `json:"description"`
+		Coin              uint                    `json:"coin"`
+		Name              string                  `json:"name"`
+		Version           string                  `json:"nft_version"`
+		TransferFee       *CollectibleTransferFee `json:"transfer_fee,omitempty"`
+		OriginalSourceURL CollectibleMediaURL     `json:"original_source_url"`
+		PreviewImageURL   CollectibleMediaURL     `json:"preview_image_url"`
 	}
 
 	CollectiblePage []Collectible
