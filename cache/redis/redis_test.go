d384d569c38c12f9422a05696c4a4fdbb850ec63
Ivan Bozhytksyi <82358585+ivan-bozhytksyi@users.noreply.github.com>
2021-04-23 11:12:43 +0300

    Upgrade redis version & implement batch get and set functions (#59)
    * Upgrade redis version & implement batch get and set functions
    
    * Fix tests

--- cache/redis/redis_test.go
+++ cache/redis/redis_test.go
@@ -1,6 +1,8 @@
 package redis
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
@@ -24,18 +26,52 @@ func TestRedis_Set(t *testing.T) {
 		F     float64
 	}{"1", 200.1}
 
-	err = r.Set("test", testData, time.Second)
+	err = r.Set(context.TODO(), "test", testData, time.Second)
 	assert.Nil(t, err)
 
 	var newValue struct {
 		Field string
 		F     float64
 	}
-	err = r.Get("test", &newValue)
+	err = r.Get(context.TODO(), "test", &newValue)
 	assert.Nil(t, err)
 	assert.Equal(t, testData, newValue)
 
-	ttl := r.client.TTL("test")
+	ttl := r.client.TTL(context.TODO(), "test")
+	assert.Equal(t, time.Second, ttl.Val())
+}
+
+func TestRedis_MSet_MGet(t *testing.T) {
+	r, err := redisInit(t)
+	assert.Nil(t, err)
+
+	type testDataType struct {
+		Field string
+		F     float64
+	}
+	testData := map[string]interface{}{
+		"test1": testDataType{"1", 200.1},
+		"test2": testDataType{"1", 200.1},
+	}
+
+	err = r.MSet(context.TODO(), testData, time.Second)
+	assert.Nil(t, err)
+
+	keys := []string{"test1", "test2"}
+	values, err := r.MGet(context.TODO(), keys...)
+	assert.Nil(t, err)
+	assert.Equal(t, len(keys), len(values))
+
+	for i, key := range keys {
+		var data testDataType
+		err := json.Unmarshal(values[i], &data)
+		assert.NoError(t, err)
+		assert.Equal(t, testData[key], data)
+	}
+
+	ttl := r.client.TTL(context.TODO(), "test1")
+	assert.Equal(t, time.Second, ttl.Val())
+	ttl = r.client.TTL(context.TODO(), "test2")
 	assert.Equal(t, time.Second, ttl.Val())
 }
 
@@ -49,19 +85,19 @@ func TestRedis_Get(t *testing.T) {
 	}
 	testData := TestStruct{"1", 200.1}
 
-	err = r.Set("test", testData, time.Second)
+	err = r.Set(context.TODO(), "test", testData, time.Second)
 	assert.Nil(t, err)
 
 	var newValue TestStruct
-	err = r.Get("test", &newValue)
+	err = r.Get(context.TODO(), "test", &newValue)
 	assert.Nil(t, err)
 	assert.Equal(t, testData, newValue)
 
-	ttl := r.client.TTL("test")
+	ttl := r.client.TTL(context.TODO(), "test")
 	assert.Equal(t, time.Second, ttl.Val())
 
 	var empty interface{}
-	err = r.Get("1", empty)
+	err = r.Get(context.TODO(), "1", empty)
 	assert.NotNil(t, err)
 	assert.Nil(t, empty)
 }
@@ -69,14 +105,14 @@ func TestRedis_Get(t *testing.T) {
 func TestRedis_Delete(t *testing.T) {
 	r, err := redisInit(t)
 	assert.Nil(t, err)
-	err = r.Set("test", []byte{0, 1}, time.Second)
+	err = r.Set(context.TODO(), "test", []byte{0, 1}, time.Second)
 	assert.Nil(t, err)
 
-	err = r.Delete("test")
+	err = r.Delete(context.TODO(), "test")
 	assert.Nil(t, err)
 
 	var v []byte
-	err = r.Get("test", &v)
+	err = r.Get(context.TODO(), "test", &v)
 	assert.NotNil(t, err)
 	assert.Equal(t, string([]byte{}), string(v))
 }
@@ -85,7 +121,7 @@ func TestRedis_IsAvailable(t *testing.T) {
 	r, err := redisInit(t)
 	assert.Nil(t, err)
 
-	assert.True(t, r.IsAvailable())
+	assert.True(t, r.IsAvailable(context.TODO()))
 }
 
 func TestRedis_Reconnect(t *testing.T) {
@@ -96,7 +132,7 @@ func TestRedis_Reconnect(t *testing.T) {
 	assert.NotNil(t, mr)
 	assert.Nil(t, err)
 
-	err = r.Reconnect(fmt.Sprintf("redis://%s", mr.Addr()))
+	err = r.Reconnect(context.TODO(), fmt.Sprintf("redis://%s", mr.Addr()))
 	assert.NoError(t, err)
 }
 
@@ -105,7 +141,7 @@ func redisInit(t *testing.T) (*Redis, error) {
 	assert.NotNil(t, mr)
 	assert.Nil(t, err)
 
-	c, err := Init(fmt.Sprintf("redis://%s", mr.Addr()))
+	c, err := Init(context.TODO(), fmt.Sprintf("redis://%s", mr.Addr()))
 	assert.Nil(t, err)
 	assert.NotNil(t, c)
 
--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.15
 require (
 	github.com/alicebob/miniredis/v2 v2.14.3
 	github.com/deckarep/golang-set v1.7.1
-	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.8.2
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/onsi/ginkgo v1.16.1 // indirect
 	github.com/onsi/gomega v1.11.0 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/shopspring/decimal v1.2.0
-	github.com/stretchr/testify v1.6.1
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.4.0
