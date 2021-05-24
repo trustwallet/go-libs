47f97bbf73b9e40b3d388aa901601def104b2b78
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2021-05-24 14:04:28 -0700

    Remove ExternalCoin

--- coin/models.go
+++ coin/models.go
@@ -2,22 +2,6 @@ package coin
 
 import "errors"
 
-type ExternalCoin struct {
-	Coin     uint   `json:"coin"`
-	Symbol   string `json:"symbol"`
-	Name     string `json:"name"`
-	Decimals uint   `json:"decimals"`
-}
-
-func (c *Coin) External() *ExternalCoin {
-	return &ExternalCoin{
-		Coin:     c.ID,
-		Name:     c.Name,
-		Symbol:   c.Symbol,
-		Decimals: c.Decimals,
-	}
-}
-
 func GetCoinForId(id string) (Coin, error) {
 	for _, c := range Coins {
 		if c.Handle == id {
