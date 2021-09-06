1f3269a0d33dbca27d4eb3ab5c210ad0c1a55d4c
Ivan Bozhytskyi <82358585+ivan-bozhytksyi@users.noreply.github.com>
2021-09-06 14:32:28 +0300

    [gin] add unix file graceful shutdown listener

--- gin/setup.go
+++ gin/setup.go
@@ -2,6 +2,7 @@ package gin
 
 import (
 	"context"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -40,4 +41,52 @@ func SetupGracefulShutdown(ctx context.Context, port string, engine *gin.Engine)
 	stop := <-signalForExit
 	log.Info("Stop signal Received", stop)
 	log.Info("Waiting for all jobs to stop")
+}
+
+// SetupGracefulServeWithUnixFile blocks execution until interruption command sent
+func SetupGracefulServeWithUnixFile(ctx context.Context, engine *gin.Engine, unixFile string) {
+	_, err := os.Create("/tmp/app-initialized")
+	if err != nil {
+		log.WithError(err).Error("failed to create file /tmp/app-initialized")
+		return
+	}
+
+	defer func() {
+		if err != nil {
+			log.Error(err)
+		}
+	}()
+
+	listener, err := net.Listen("unix", unixFile)
+	if err != nil {
+		return
+	}
+
+	defer func() { _ = listener.Close() }()
+	defer func() { _ = os.Remove(unixFile) }()
+
+	server := &http.Server{
+		Handler: engine,
+	}
+
+	defer func() {
+		if err := server.Shutdown(ctx); err != nil {
+			log.Info("Server Shutdown: ", err)
+		}
+	}()
+
+	signalForExit := make(chan os.Signal, 1)
+	signal.Notify(signalForExit,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	go func() {
+		log.Debugf("Listening and serving HTTP on unix:/%s", unixFile)
+		err = server.Serve(listener)
+	}()
+
+	stop := <-signalForExit
+	log.Info("Stop signal Received ", stop)
 }
