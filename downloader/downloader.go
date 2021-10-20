28c1e2233bc365f2397c2bbcb2017a93daa9acbb
dimkouv <dimkouv@protonmail.com>
2021-10-20 14:18:41 +0300

    init downloader

---
+++ downloader/downloader.go
@@ -0,0 +1,69 @@
+package downloader
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Interface interface {
+	Download(url string) ([]byte, error)
+}
+
+type dl struct {
+	client     http.Client
+	bytesLimit int64
+}
+
+func (d *dl) Download(url string) ([]byte, error) {
+	resp, err := d.client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if !(resp.StatusCode >= 200 && resp.StatusCode <= 299) {
+		return nil, fmt.Errorf("response status code: %d", resp.StatusCode)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.WithField("error", err).Error("cannot close request body")
+		}
+	}()
+
+	reader, _ := resp.Body.(io.Reader)
+	if d.bytesLimit > 0 {
+		reader = &io.LimitedReader{R: resp.Body, N: d.bytesLimit}
+	}
+
+	b, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+type Option func(d *dl) error
+
+func New(opts ...Option) (Interface, error) {
+	d := &dl{
+		client: http.Client{},
+	}
+
+	for _, opt := range opts {
+		if err := opt(d); err != nil {
+			return nil, err
+		}
+	}
+
+	return d, nil
+}
+
+func OptionBytesLimit(bytesLimit int64) Option {
+	return func(d *dl) error {
+		d.bytesLimit = bytesLimit
+		return nil
+	}
+}
