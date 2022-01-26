e6f31cce19c77e0d17f7d1010c861c42ff3dcfc3
dimkouv <dimkouv@protonmail.com>
2022-01-26 14:55:28 +0200

    refactor nullable package + include more data types

--- pkg/nullable/primitives.go
+++ pkg/nullable/primitives.go
@@ -2,7 +2,6 @@ package nullable
 
 import (
 	"fmt"
-	"time"
 )
 
 func String(s string) *string {
@@ -18,14 +17,50 @@ func Int(i int) *int {
 	return &i
 }
 
+func Int8(i int8) *int8 {
+	return &i
+}
+
+func Int16(i int16) *int16 {
+	return &i
+}
+
+func Int32(i int32) *int32 {
+	return &i
+}
+
 func Int64(i int64) *int64 {
 	return &i
 }
 
-func Bool(b bool) *bool {
-	return &b
+func Uint(i uint) *uint {
+	return &i
 }
 
-func Time(t time.Time) *time.Time {
-	return &t
+func Uint8(i uint8) *uint8 {
+	return &i
+}
+
+func Uint16(i uint16) *uint16 {
+	return &i
+}
+
+func Uint32(i uint32) *uint32 {
+	return &i
+}
+
+func Uint64(i uint64) *uint64 {
+	return &i
+}
+
+func Float32(f float32) *float32 {
+	return &f
+}
+
+func Float64(f float64) *float64 {
+	return &f
+}
+
+func Bool(b bool) *bool {
+	return &b
 }
