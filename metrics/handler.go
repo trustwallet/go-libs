d6cd2477012b11259c00d312b6bdb5856059d4b4
Dmytro Vorotyntsev <dmytro.vorotyntsev@gmail.com>
2022-01-28 14:56:31 +0200

    minor update

--- metrics/handler.go
+++ metrics/handler.go
@@ -5,6 +5,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func Handler(engine *gin.Engine, path string) {
+func InitHandler(engine *gin.Engine, path string) {
 	engine.GET(path, gin.WrapH(promhttp.Handler()))
 }
