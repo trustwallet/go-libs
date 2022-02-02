24031ef081d5e26ffcbbfca26ce4166386137c21
daniel <24758309+unanoc@users.noreply.github.com>
2022-02-03 00:59:59 +0300

    Add new api server (#134)
    * Add new api server
    
    * Fix according to review

--- gin/setup.go
+++ gin/setup.go
@@ -6,12 +6,71 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
+type Server interface {
+	Run(ctx context.Context, wg *sync.WaitGroup)
+}
+
+type api struct {
+	router http.Handler
+	port   string
+}
+
+func NewHTTPServer(router http.Handler, port string) Server {
+	return &api{
+		router: router,
+		port:   port,
+	}
+}
+
+func (a *api) Run(ctx context.Context, wg *sync.WaitGroup) {
+	a.serve(ctx, wg)
+}
+
+func (a *api) serve(ctx context.Context, wg *sync.WaitGroup) {
+	wg.Add(1)
+
+	server := &http.Server{
+		Addr:    ":" + a.port,
+		Handler: a.router,
+	}
+
+	serverStopped := make(chan struct{})
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Error("Server ListenAndServe: ", err)
+			serverStopped <- struct{}{}
+		}
+	}()
+
+	log.WithFields(log.Fields{"bind": a.port}).Info("Starting the API server")
+
+	go func() {
+		defer func() { wg.Done() }()
+
+		select {
+		case <-ctx.Done():
+			log.Info("Shutting down the server")
+
+			if err := server.Shutdown(context.Background()); err != nil {
+				log.Info("Server Shutdown: ", err)
+			}
+
+			return
+		case <-serverStopped:
+			return
+		}
+	}()
+}
+
+// Deprecated
 // SetupGracefulShutdown blocks execution until interruption command sent
 func SetupGracefulShutdown(ctx context.Context, port string, engine *gin.Engine) {
 	server := &http.Server{
