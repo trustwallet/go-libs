6241f1dbdf14d87a128453f79d3ca66e07664637
vorotech <2937451+vorotech@users.noreply.github.com>
2022-04-14 16:50:04 +0300

    metrics pusher worker

---
+++ worker/metrics/metricspusherworker.go
@@ -0,0 +1,12 @@
+package metrics
+
+import (
+	"github.com/trustwallet/go-libs/metrics"
+	"github.com/trustwallet/go-libs/worker"
+)
+
+func NewMetricsPusherWorker(options *worker.WorkerOptions, pusher metrics.Pusher) worker.Worker {
+	w := worker.InitWorker("metrics_pusher", options, pusher.Push)
+	w.(worker.DisposableWorker).WithStop(pusher.Close)
+	return w
+}
