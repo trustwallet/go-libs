86ab53d4d3ce7934456e8ed6fe13ba19a76763d6
nikoskarakostas <nikoskarakostas@gmail.com>
2022-11-01 15:25:00 +0200

    extend ordered set with Size and ValueAt methods

--- set/ordered_test.go
+++ set/ordered_test.go
@@ -14,6 +14,14 @@ func TestContains(t *testing.T) {
 		u.Add(5)
 		u.Add(8)
 		assert.Equal(t, []int{1, 5, 8}, u.Values())
+
+		assert.Equal(t, 3, u.Size())
+
+		assert.Equal(t, 1, u.ValueAt(0, 0))
+		assert.Equal(t, 5, u.ValueAt(1, 0))
+		assert.Equal(t, 8, u.ValueAt(2, 0))
+		assert.Equal(t, 0, u.ValueAt(256, 0))
+		assert.Equal(t, 0, u.ValueAt(-1, 0))
 	})
 
 	t.Run("str", func(t *testing.T) {
@@ -23,5 +31,12 @@ func TestContains(t *testing.T) {
 		u.Add("baz")
 		u.Add("foo")
 		assert.Equal(t, []string{"foo", "bar", "baz"}, u.Values())
+
+		assert.Equal(t, 3, u.Size())
+
+		assert.Equal(t, "foo", u.ValueAt(0, ""))
+		assert.Equal(t, "bar", u.ValueAt(1, ""))
+		assert.Equal(t, "baz", u.ValueAt(2, ""))
+		assert.Equal(t, "", u.ValueAt(256, ""))
 	})
 }
