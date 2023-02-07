2ae056e58a124634b21f48bbc24a8117e40726df
iCell <shawn.lee@binance.com>
2023-02-07 10:49:57 +0900

    unified initialization method

--- database/db.go
+++ database/db.go
@@ -21,16 +21,17 @@ type transactionKey struct{}
 
 var trxKey = &transactionKey{}
 
+// DBGetter implements the DBContextGetter interface, allowing retrieval of a read/write database connection.
 type DBGetter struct {
 	readWriteDb *gorm.DB
 	readOnlyDb  *gorm.DB
 }
 
-func NewDbWrapper(db *gorm.DB) *DBGetter {
-	return &DBGetter{readWriteDb: db}
-}
-
-func NewReadWriteDbWrapper(reader, writer *gorm.DB) *DBGetter {
+// NewDbWrapper creates DBGetter instance.
+// Both of reader and writer should not be empty, they can be the same pointer pointing to the same database connection.
+// reader == writer, in which case they represent a shared read-write database connection.
+// reader != writer, then they represent separate read and write database connections for read-write splitting.
+func NewDbWrapper(reader, writer *gorm.DB) *DBGetter {
 	return &DBGetter{
 		readWriteDb: writer,
 		readOnlyDb:  reader,
@@ -55,7 +56,7 @@ func (wrapper *DBGetter) Transaction(ctx context.Context, fc func(ctx context.Co
 }
 
 func (wrapper *DBGetter) Close() error {
-	if wrapper.readOnlyDb != nil {
+	if wrapper.readOnlyDb != wrapper.readWriteDb {
 		readOnlyDb, err := wrapper.readOnlyDb.DB()
 		if err != nil {
 			return err
