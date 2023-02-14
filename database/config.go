245db71787f21d61df0c3b41c4bb497bfcc7968f
Shawn Lee <shawn.lee@binance.com>
2023-02-14 14:53:58 +0900

    Use gorm `dbresolver` to implement read-write splitting (#212)
    * use gorm dbresolver to implement read-write splitting
    
    * format
    
    * fix linter issue
    
    * remove unused file
    
    * provide GetSourceDB() function
    
    * add comment

---
+++ database/config.go
@@ -0,0 +1,81 @@
+package database
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm/logger"
+)
+
+type LogLevel string
+
+const (
+	LogLevelSilent LogLevel = "silent"
+	LogLevelError  LogLevel = "error"
+	LogLevelWarn   LogLevel = "warn"
+	LogLevelInfo   LogLevel = "info"
+)
+
+func newLogLevelFromString(logLevel LogLevel) (logger.LogLevel, error) {
+	switch logLevel {
+	case LogLevelSilent:
+		return logger.Silent, nil
+	case LogLevelError:
+		return logger.Error, nil
+	case LogLevelWarn:
+		return logger.Warn, nil
+	case LogLevelInfo:
+		return logger.Info, nil
+	default:
+		return 0, fmt.Errorf("invalid log level")
+	}
+}
+
+type DBConnPool struct {
+	MaxIdleConns    int           `mapstructure:"max_idle_conns"`
+	ConnMaxIdleTime time.Duration `mapstructure:"conn_max_idle_time"`
+	MaxOpenConns    int           `mapstructure:"max_open_conns"`
+	ConnMaxLifetime time.Duration `mapstructure:"conn_max_lifetime"`
+}
+
+// DBConfig represents the configuration for a database connection.
+type DBConfig struct {
+	// Url is the URL of the read-write database instance to connect to.
+	Url string `mapstructure:"url"`
+
+	// ReadonlyUrl is the URL of the read-only database instances.
+	// This is optional and can be set to nil if read-write splitting is not required.
+	ReadonlyUrl *string `mapstructure:"readonly_url"`
+
+	// LogLevel is the logging level for the database connection.
+	// Possible values are "silent", "error", "warn", and "info".
+	// This is optional and the default value is "error".
+	LogLevel LogLevel `mapstructure:"log_level"`
+
+	// ConnPool is the connection pool settings for the database connection.
+	// This is optional and can be set to nil if the default connection pool settings are sufficient.
+	ConnPool *DBConnPool `mapstructure:"conn_pool"`
+}
+
+var (
+	defaultMaxIdleConns    = 2
+	defaultMaxOpenConns    = 0
+	defaultConnMaxIdleTime = time.Duration(0)
+	defaultConnMaxLifetime = time.Duration(0)
+)
+
+func (cfg *DBConfig) applyDefaultValue() {
+	if cfg.LogLevel == "" {
+		cfg.LogLevel = LogLevelError
+	}
+	if cfg.ConnPool == nil {
+		// match the default configuration in database/sql
+		// https: //github.com/golang/go/blob/198074abd7ec36ee71198a109d98f1ccdb7c5533/src/database/sql/sql.go#L912
+		cfg.ConnPool = &DBConnPool{
+			MaxIdleConns:    defaultMaxIdleConns,
+			ConnMaxIdleTime: defaultConnMaxIdleTime,
+			MaxOpenConns:    defaultMaxOpenConns,
+			ConnMaxLifetime: defaultConnMaxLifetime,
+		}
+	}
+}
--- database/db.go
+++ database/db.go
@@ -2,6 +2,13 @@ package database
 
 import (
 	"context"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm/logger"
+	"gorm.io/plugin/dbresolver"
 
 	"gorm.io/gorm"
 )
@@ -23,43 +30,85 @@ var trxKey = &transactionKey{}
 
 // DBGetter implements the DBContextGetter interface, allowing retrieval of a read/write database connection.
 type DBGetter struct {
-	readWriteDb *gorm.DB
-	readOnlyDb  *gorm.DB
+	db *gorm.DB
 }
 
-// NewDBGetter creates DBGetter instance.
-// Both of reader and writer should not be empty, they can be the same pointer pointing to the same database connection.
-// reader == writer, in which case they represent a shared read-write database connection.
-// reader != writer, then they represent separate read and write database connections for read-write splitting.
-func NewDBGetter(reader, writer *gorm.DB) *DBGetter {
-	return &DBGetter{
-		readWriteDb: writer,
-		readOnlyDb:  reader,
+// NewDBGetter creates a new DBGetter instance with the specified database configuration.
+// If you are using a read-write splitting database connection, the `dbresolver` will automatically select
+// the appropriate connection based on the SQL to be executed.
+// When using database transactions, the read-write connection is used by default.
+// If you want to force the use of write or read connection, you can use the following method:
+// ```
+//
+//	getter.DBFrom(ctx).Clauses(dbresolver.Write/dbresolver.Read)
+//
+// ```
+// For more information, read https://gorm.io/docs/dbresolver.html#Read-x2F-Write-Splitting
+// *Note* that when using read-write splitting, there is a potential issue where a read operation immediately
+// following a write operation may not see the updated data if it is executed on a different read-only replica
+// that has not yet been updated with the new data.
+func NewDBGetter(cfg DBConfig) (*DBGetter, error) {
+	cfg.applyDefaultValue()
+
+	logLevel, err := newLogLevelFromString(cfg.LogLevel)
+	if err != nil {
+		return nil, err
 	}
+
+	db, err := gorm.Open(postgres.Open(cfg.Url), &gorm.Config{
+		SkipDefaultTransaction: true,
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logLevel,
+				IgnoreRecordNotFoundError: true,
+				Colorful:                  true,
+			}),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var replicas []gorm.Dialector
+	if cfg.ReadonlyUrl != nil {
+		replicas = append(replicas, postgres.Open(*cfg.ReadonlyUrl))
+	}
+
+	resolver := dbresolver.Register(
+		dbresolver.Config{
+			Sources:           []gorm.Dialector{postgres.Open(cfg.Url)},
+			Replicas:          replicas,
+			TraceResolverMode: logLevel == logger.Info,
+		}).SetConnMaxIdleTime(cfg.ConnPool.ConnMaxIdleTime).
+		SetConnMaxLifetime(cfg.ConnPool.ConnMaxLifetime).
+		SetMaxIdleConns(cfg.ConnPool.MaxIdleConns).
+		SetMaxOpenConns(cfg.ConnPool.MaxOpenConns)
+	if err := db.Use(resolver); err != nil {
+		return nil, err
+	}
+	return &DBGetter{db: db}, nil
+}
+
+func (getter *DBGetter) GetSourceDB() *gorm.DB {
+	return getter.db
 }
 
 func (getter *DBGetter) HealthCheck() error {
-	for _, db := range getter.allDBs() {
-		sqlDB, err := db.DB()
-		if err != nil {
-			return err
-		}
-		if err := sqlDB.Ping(); err != nil {
-			return err
-		}
+	// gorm dbresolver doesn't support getting replica connection
+	// https://github.com/go-gorm/dbresolver/issues/45
+	sqlDB, err := getter.db.DB()
+	if err != nil {
+		return err
 	}
-	return nil
+	return sqlDB.Ping()
 }
 
 func (getter *DBGetter) DBFrom(ctx context.Context) *gorm.DB {
 	if db, ok := ctx.Value(trxKey).(*gorm.DB); ok {
 		return db
 	}
-	return getter.readWriteDb
-}
-
-func (getter *DBGetter) ReadOnlyDB() *gorm.DB {
-	return getter.readOnlyDb
+	return getter.db
 }
 
 func (getter *DBGetter) Transaction(ctx context.Context, fc func(ctx context.Context) error) error {
@@ -69,22 +118,11 @@ func (getter *DBGetter) Transaction(ctx context.Context, fc func(ctx context.Con
 }
 
 func (getter *DBGetter) Close() error {
-	for _, db := range getter.allDBs() {
-		sqlDB, err := db.DB()
-		if err != nil {
-			return err
-		}
-		if err := sqlDB.Close(); err != nil {
-			return err
-		}
+	// gorm dbresolver doesn't support getting replica connection
+	// https://github.com/go-gorm/dbresolver/issues/45
+	sqlDB, err := getter.db.DB()
+	if err != nil {
+		return err
 	}
-	return nil
-}
-
-func (getter *DBGetter) allDBs() []*gorm.DB {
-	result := []*gorm.DB{getter.readWriteDb}
-	if getter.readOnlyDb != getter.readWriteDb {
-		result = append(result, getter.readOnlyDb)
-	}
-	return result
+	return sqlDB.Close()
 }
