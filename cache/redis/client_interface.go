1d4cb3d81f4d8a1fd75e46ccd2d5dd76b61d65b2
qct <tango.qct@gmail.com>
2023-09-14 07:04:08 +0400

    Add SetNX SetXX (#236)
    * Add SetNX
    
    * Add SetXX

--- cache/redis/client_interface.go
+++ cache/redis/client_interface.go
@@ -16,6 +16,8 @@ type redisClient interface {
 	Del(ctx context.Context, keys ...string) *redis.IntCmd
 	Pipeline() redis.Pipeliner
 	Watch(ctx context.Context, fn func(tx *redis.Tx) error, keys ...string) error
+	SetNX(ctx context.Context, key string, value interface{}, expiration time.Duration) *redis.BoolCmd
+	SetXX(ctx context.Context, key string, value interface{}, expiration time.Duration) *redis.BoolCmd
 
 	Ping(ctx context.Context) *redis.StatusCmd
 	Scan(ctx context.Context, cursor uint64, match string, count int64) *redis.ScanCmd
